0311d35ff629d5752d459873bbde3e272a447e1e
Kortney Mercy <kmercy@pr-d3.kzn.21-school.ru>
2021-12-11 18:06:24 +0300

    sasha228

--- PAPKA_FOR_SANYA/unset.c
+++ PAPKA_FOR_SANYA/unset.c
@@ -6,31 +6,17 @@
 /*   By: eveiled <eveiled@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/29 18:36:25 by eveiled           #+#    #+#             */
-/*   Updated: 2021/12/11 17:04:39 by eveiled          ###   ########.fr       */
+/*   Updated: 2021/12/11 17:47:48 by kmercy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell_2.h"
 
-//int	count_argv(char **argv)
-//{
-//	int	size;
-//	char	**save;
-//
-//	save = argv;
-//	size = 0;
-//	while (save[size])
-//		size++;
-//	return (size);
-//}
-
 void	ft_unset(char **args, char ***env_array)
 {
 	int		i;
-	int		argc;
+	int 	j;
 	char	**tmp;
-	int 	arg_len;
-	int 	envp_len;
 
 	if (ft_arr_len(args) <= 1)
 	{
@@ -42,14 +28,13 @@ void	ft_unset(char **args, char ***env_array)
 	{
 		tmp = *env_array;
 		*env_array = ft_calloc(ft_arr_len(tmp) + 1, sizeof (char *));
-		i = -1;
-		while ((tmp)[++i])
+		i = 0;
+		j = -1;
+		while ((tmp)[++j])
 		{
-			envp_len = ft_strlen(tmp[i]) - ft_strlen(ft_strchr(tmp[i], '='));
-			arg_len = ft_strlen(args[1]);
-			printf("%d   %d  %d \n", envp_len, arg_len, ft_strncmp(tmp[i], args[1], envp_len));
-			if (!((!ft_strncmp(tmp[i], args[1], envp_len)) && envp_len == arg_len))
-				(*env_array)[i] = ft_strdup(tmp[i]);
+			if (!(ft_strncmp(tmp[j], args[1], ft_strlen(tmp[j]) - ft_strlen(ft_strchr(tmp[j], '='))) == 0
+			&& ft_strlen(tmp[j]) - ft_strlen(ft_strchr(tmp[j], '=')) == ft_strlen(args[1])))
+				(*env_array)[i++] = ft_strdup(tmp[j]);
 		}
 		(*env_array)[i] = NULL;
 		free_all(tmp);
